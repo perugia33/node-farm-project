f99697ac9f21b437c4e4814e7351e74bca080161
Theresa Davis <perugia33@gmail.com>
2023-11-05 08:15:05 -0800

    Updated overview & routes

--- index.js
+++ index.js
@@ -4,22 +4,70 @@ const url = require('url');
 
 
 //  Server
+
+const replaceTemplateItem = (temp, product)=> {
+    let output = temp.replace(/{%PRODUCTNAME%}/g,   product.productName);
+    output = output.replace(/{%IMAGE%}/g,   product.image);
+    output = output.replace(/{%PRICE%}/g,   product.price);
+    output = output.replace(/{%LOCATION%}/g,  product.from);
+    output = output.replace(/{%NUTRIENTS%}/g,   product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g,   product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g,   product.description);
+    output = output.replace(/{%ID%}/g,   product.id);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,   'not-organic');
+    return output;
+}
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,  'utf-8' );
+const tempProduct = fs.readFileSync( `${__dirname}/templates/template-product.html`, 'utf-8' );
+const tempCard = fs.readFileSync( `${__dirname}/templates/template-card.html`, 'utf-8' );
+
+const styleCSS= fs.readFileSync( `${__dirname}/style.css`, 'utf-8' );
+
+
 const data = fs.readFileSync('./json-data/data.json', 'utf-8');
 const dataObj = JSON.parse(data);
   
 
  const server = http.createServer((req, res) =>{
+    console.log("This is parse.url", url.parse(req.url, true));
+    console.log('This is req.url',  req.url);
     const pathName = req.url;
 
-    if(pathName ===  '/'  || pathName === '/overview'){
-        res.end("This is the Overview");
+
+
+    // CSS Stylesheet
+
+    if(pathName ===  '/style.css'){
+        res.writeHead(200,  { 'Content-type': 'text/css' });
+        res.end(styleCSS);
+
+    // Overview Page
+
+     }else if(pathName ===  '/'  || pathName === '/overview'){
+        res.writeHead(200,  { 'Content-type': 'text/html' });
+
+        const cardsHtml = dataObj.map(el => replaceTemplateItem(tempCard, el)).join( ' ');
+        // creates an array with json data which is convert into one string
+      
+         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);      
+       
+         res.end(output);
+
+    // Product Page
+
     } else if (pathName === '/product'){
         res.end("This is the Product");
+
+     // API
 
     } else if(pathName === '/api'){
             res.writeHead(200,  {
                 'Content-type': 'application/json' });
                 res.end(data);
+
+     //  Not Found
+
     } else{
         res.writeHead(404, { 
             'Content-type': 'text/html'
